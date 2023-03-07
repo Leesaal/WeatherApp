4d04c7b90db614471a14a021aad02f7cf0e39316
Leesa A <leesaali7@gmail.com>
2023-03-07 23:02:17 +0000

    added html elements and css styling

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// create variables to select
+var searchButton = $(".searchButton");
+var searchedCities = [];
+
+// Access to open weather API
+var APIKey = "e95b7c930467798b718473602a6561c5";
+
+
+// create event listeners
+searchButton.click(function () {
+
+    var searchCity = $(".searchCity").val();
+    searchedCities.push(searchCity);
+// set Items by identifying a key to set the search city to
+    
+
+})
+
