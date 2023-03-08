4f697bffc3fd62e63c25217f4c50361ceed5d2bb
Leesa A <leesaali7@gmail.com>
2023-03-08 19:15:05 +0000

    added code to fetch current weather

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // create variables to select
 var searchButton = $(".searchButton");
 var searchedCities = [];
+var city = $("#city");
 
 // Access to open weather API
 var APIKey = "e95b7c930467798b718473602a6561c5";
@@ -16,3 +17,22 @@ searchButton.click(function () {
 
 })
 
+
+var weather = {
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&appid=" 
+        + APIKey
+    )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+        var { name } = data;
+        var { icon, description } = data.weather[0];
+        var { temp, humidity } = data.main;
+        var { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+    }
+};
