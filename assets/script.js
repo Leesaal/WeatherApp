d47a85c95de63fc649ea2651b423b054d23838f6
Leesa A <leesaali7@gmail.com>
2023-03-09 09:55:14 +0000

    added fetch for long and lat

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,7 @@ searchButton.click(function () {
 
 
 var weather = {
+ 
     fetchWeather: function(city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
@@ -26,13 +27,39 @@ var weather = {
         + APIKey
     )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
+        
     },
+
     displayWeather: function(data) {
         var { name } = data;
         var { icon, description } = data.weather[0];
         var { temp, humidity } = data.main;
         var { speed } = data.wind;
         console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"
+        + icon
+        + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    },
+
+    futureWeather: function(city) {
+        fetch("http://api.openweathermap.org/geo/1.0/direct?q="
+        + city
+        + "&limit=5&appid=" 
+        + APIKey
+    )
+        .then((response) => response.json())
+        .then((data) => this.fetchFutureWeather(data));
+    },
+
+    fetchFutureWeather: function(data) {
+        var { lon } = data[0];
+        var { lat } = data[0];
+        console.log(lon + ", " + lat);
     }
 };
